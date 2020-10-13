84fdb9b699d3c631334315d8d6fbc01214c3548c
Vital Nahorny <vital.nagorny@gmail.com>
2020-10-13 18:29:27 +0300

    feat: decided

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const brackets = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        brackets.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    }
+    let preStr = str;
+    let curStr = str;
+    while (true) {
+        for (let i = 0; i < brackets.length; i++) {
+            curStr = curStr.replace(brackets[i], '');
+        } if (curStr.length == 0) {
+            return true;
+        } if (preStr === curStr) {
+            return false;
+        } preStr = curStr;
+    }
 }
